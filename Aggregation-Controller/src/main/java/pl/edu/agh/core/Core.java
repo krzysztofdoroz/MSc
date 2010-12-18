b72c3dad99e402256b73f3e29624f38460b43415
Krzysztof Doroz <krzysztofdoroz@gmail.com>
2010-12-18 21:32:13 +0100

    log4 support for Aggregation-Controller added

--- Aggregation-Controller/src/main/java/pl/edu/agh/core/Core.java
+++ Aggregation-Controller/src/main/java/pl/edu/agh/core/Core.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.core;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.FileSystemResource;
@@ -8,13 +10,18 @@ import org.springframework.jms.core.JmsTemplate;
 
 public class Core {
 
+	static Logger logger = Logger.getLogger(Core.class);
+	
 	public static void main(String[] args) {
 
+		PropertyConfigurator.configure("log4j.properties");
 		Resource resource = new FileSystemResource("context.xml");
 		BeanFactory factory = new XmlBeanFactory(resource);
 
 		JmsTemplate jmsTemplate = (JmsTemplate) factory.getBean("jmsTemplate");
 
+		logger.info("asasdaf");
+		
 		jmsTemplate.convertAndSend("simple","ala ma kota");
 	}
 
