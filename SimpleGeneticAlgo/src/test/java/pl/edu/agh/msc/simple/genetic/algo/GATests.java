57bca9d8e8c4ece4e9a821cded54ba74370320da
Krzysztof Doroz <krzysztofdoroz@gmail.com>
2011-02-10 18:31:17 +0100

    simple genetic algo total portfolio value function added

--- SimpleGeneticAlgo/src/test/java/pl/edu/agh/msc/simple/genetic/algo/GATests.java
+++ SimpleGeneticAlgo/src/test/java/pl/edu/agh/msc/simple/genetic/algo/GATests.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.msc.simple.genetic.algo;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,6 +32,13 @@ public class GATests {
 	public void testGA(){
 		
 		double money = 100.0;
+		double[] stocskBought = new double[2];
+		BufferedWriter bufferedWriter = null;
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(new File("results")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		System.out.println("GA test:");
 		GeneticAlgorithmImpl gen = new GeneticAlgorithmImpl(2,16, 0.2, 0.1, 0.3, dataSource);
@@ -35,6 +46,26 @@ public class GATests {
 		for(int i = 0; i < 253 ; i++){
 			Portfolio bestPortfolio = gen.calculateCurrentPortfolio();
 
+			if (stocskBought[0] + stocskBought[1] > 0 ){
+				money = 0;
+				int index = 0;
+				for(double ammount: stocskBought){
+					money += ammount * dataSource.getStockData(index, i);
+					index++;
+				}
+			}
+			
+			System.out.println(money);
+			try {
+				bufferedWriter.write(i + " " + money + " " + stocskBought[0] + " " + stocskBought[1] + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			//buy stocks according to best portfolio
+			stocskBought[0] = (bestPortfolio.getPortfolio().get(0) * money) / dataSource.getStockData(0, i);
+			stocskBought[1] = (bestPortfolio.getPortfolio().get(1) * money) / dataSource.getStockData(1, i);
+			
 			System.out.println("      CURRENT POPULATION STATE:  ");
 			
 			for(Portfolio portfolio : gen.getPopulation()){
@@ -42,11 +73,16 @@ public class GATests {
 			}
 			System.out.println("  BEST PORTFOLIO:");
 
-			money *= bestPortfolio.getValue();
+			//money *= bestPortfolio.getValue();
 			System.out.println(bestPortfolio);
 			System.out.println("TOTAL MONEY:" + money);
 			
 			System.out.println("----------------------------------------------------");
+		}
+		try {
+			bufferedWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
