7a8f61d4d70a6664df7372404ff813ed4ddd99bf
Krzysztof Doroz <krzysztofdoroz@gmail.com>
2011-03-12 00:06:59 +0100

    aggregation controller aggregates all data from computing nodes; plotting charts possible

--- Aggregation-Controller/src/main/java/pl/edu/agh/agg/controller/impl/AggregationControllerImpl.java
+++ Aggregation-Controller/src/main/java/pl/edu/agh/agg/controller/impl/AggregationControllerImpl.java
@@ -17,6 +17,7 @@ import org.springframework.jms.core.JmsTemplate;
 
 import pl.edu.agh.agg.controller.IAggregationController;
 import pl.edu.agh.msc.MPTGeneticAlgorithm.MPTPortfolio;
+import pl.edu.agh.msc.data.source.interfaces.IDataSource;
 import pl.edu.agh.msc.generic.genetic.algorithm.Portfolio;
 
 public class AggregationControllerImpl implements IAggregationController {
@@ -27,6 +28,7 @@ public class AggregationControllerImpl implements IAggregationController {
 	private JmsTemplate jmsTopicTemplate;
 	private int numberOfComputingAgents;
 	private int numberOfRounds;
+	private IDataSource dataSource;
 
 	private String queueName;
 	List<Portfolio> portfolios = new LinkedList<Portfolio>();
@@ -34,10 +36,17 @@ public class AggregationControllerImpl implements IAggregationController {
 	public void getResultsFromComputingNodes() throws JmsException {
 		logger.info("receiving results from computing nodes");
 		BufferedWriter bufferedWriter;
+
+		double money = 100.0;
+		double[] stocskBought = new double[2];
+		BufferedWriter paretoWriter = null;
+
 		try {
 			Message message;
 			bufferedWriter = new BufferedWriter(new FileWriter(new File(
 					"aggregated_results")));
+			paretoWriter = new BufferedWriter(new FileWriter(new File(
+					"pareto_results")));
 
 			for (int round = 0; round < numberOfRounds; round++) {
 				portfolios.clear();
@@ -66,9 +75,26 @@ public class AggregationControllerImpl implements IAggregationController {
 					 */
 				}
 
+				if (stocskBought[0] + stocskBought[1] > 0) {
+					money = 0;
+					int index = 0;
+					for (double ammount : stocskBought) {
+						money += ammount * dataSource.getStockData(index, round);
+						index++;
+					}
+				}
+				
 				MPTPortfolio bestPortfolio = selectBestPortfolio(portfolios);
-				bufferedWriter.write(round + " " + bestPortfolio.getRisk()
-						+ " " + bestPortfolio.getValue() + "\n");
+				bufferedWriter.write(round + " " + money + " "
+						+ stocskBought[0] + " " + stocskBought[1] + " "
+						+ bestPortfolio.getRisk() / 100.0 + " "
+						+ bestPortfolio.getValue() + "\n");
+
+				// buy stocks according to best portfolio
+				stocskBought[0] = (bestPortfolio.getPortfolio().get(0) * money)
+						/ dataSource.getStockData(0, round);
+				stocskBought[1] = (bestPortfolio.getPortfolio().get(1) * money)
+						/ dataSource.getStockData(1, round);
 
 				// request new results from computing nodes
 				for (int j = 0; j < numberOfComputingAgents; j++) {
@@ -143,5 +169,13 @@ public class AggregationControllerImpl implements IAggregationController {
 
 	public void setJmsTopicTemplate(JmsTemplate jmsTopicTemplate) {
 		this.jmsTopicTemplate = jmsTopicTemplate;
+	}
+
+	public IDataSource getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(IDataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 }
