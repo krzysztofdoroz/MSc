0d154f705d193425edc12492f53e45d28bc44991
Krzysztof Doroz <krzysztofdoroz@gmail.com>
2011-03-23 21:01:50 +0100

    new test accomodating 3 stocks

--- SimpleGeneticAlgo/src/test/java/pl/edu/agh/msc/simple/genetic/algo/GATest3Stocks.java
+++ SimpleGeneticAlgo/src/test/java/pl/edu/agh/msc/simple/genetic/algo/GATest3Stocks.java
@@ -15,67 +15,75 @@ import pl.edu.agh.msc.data.source.interfaces.IDataSource;
 import pl.edu.agh.msc.generic.genetic.algorithm.Portfolio;
 import pl.edu.agh.msc.simple.genetic.algoImpl.GeneticAlgorithmImpl;
 
-public class GATests {
+public class GATest3Stocks {
 
 	List<String> filenames;
 	IDataSource dataSource;
 
 	@Before
-	public void init(){
-		filenames =  new LinkedList<String>();
+	public void init() {
+		filenames = new LinkedList<String>();
 		filenames.add("/home/krzysztof/MSc/data-source/kghm.data");
 		filenames.add("/home/krzysztof/MSc/data-source/tpsa.data");
-		dataSource = new StockDataSource(filenames,null, null, null, null, 253);
+		filenames.add("/home/krzysztof/MSc/data-source/pko.data");
+		dataSource = new StockDataSource(filenames, null, null, null, null, 253);
 	}
-	
+
 	@Test
-	public void testGA(){
-		
+	public void testGA() {
+
 		double money = 100.0;
-		double[] stocskBought = new double[2];
+		double[] stocskBought = new double[3];
 		BufferedWriter bufferedWriter = null;
 		try {
-			bufferedWriter = new BufferedWriter(new FileWriter(new File("results")));
+			bufferedWriter = new BufferedWriter(new FileWriter(new File(
+					"results")));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println("GA test:");
-		GeneticAlgorithmImpl gen = new GeneticAlgorithmImpl(2,16, 0.2, 0.1, 0.3, dataSource);
-		
-		for(int i = 0; i < 253 ; i++){
+		GeneticAlgorithmImpl gen = new GeneticAlgorithmImpl(3, 16, 0.2, 0.1,
+				0.3, dataSource);
+
+		for (int i = 0; i < 253; i++) {
 			Portfolio bestPortfolio = gen.calculateCurrentPortfolio();
 
-			if (stocskBought[0] + stocskBought[1] > 0 ){
+			if (stocskBought[0] + stocskBought[1] + stocskBought[2] > 0) {
 				money = 0;
 				int index = 0;
-				for(double ammount: stocskBought){
+				for (double ammount : stocskBought) {
 					money += ammount * dataSource.getStockData(index, i);
 					index++;
 				}
 			}
-			
+
 			System.out.println(money);
 			try {
-				bufferedWriter.write(i + " " + money + " " + stocskBought[0] + " " + stocskBought[1] + "\n");
+				bufferedWriter.write(i + " " + money + " " + stocskBought[0]
+						+ " " + stocskBought[1] + " " + stocskBought[2] + "\n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			//buy stocks according to best portfolio
-			stocskBought[0] = (bestPortfolio.getPortfolio().get(0) * money) / dataSource.getStockData(0, i);
-			stocskBought[1] = (bestPortfolio.getPortfolio().get(1) * money) / dataSource.getStockData(1, i);
-			
+
+			// buy stocks according to best portfolio
+			stocskBought[0] = (bestPortfolio.getPortfolio().get(0) * money)
+					/ dataSource.getStockData(0, i);
+			stocskBought[1] = (bestPortfolio.getPortfolio().get(1) * money)
+					/ dataSource.getStockData(1, i);
+			stocskBought[2] = (bestPortfolio.getPortfolio().get(2) * money)
+					/ dataSource.getStockData(2, i);
+
 			System.out.println("      CURRENT POPULATION STATE:  ");
-			
-			
+
 			System.out.println("  BEST PORTFOLIO:");
 
-			//money *= bestPortfolio.getValue();
+			// money *= bestPortfolio.getValue();
 			System.out.println(bestPortfolio);
 			System.out.println("TOTAL MONEY:" + money);
-			
-			System.out.println("----------------------------------------------------");
+
+			System.out
+					.println("----------------------------------------------------");
 		}
 		try {
 			bufferedWriter.close();
@@ -83,5 +91,5 @@ public class GATests {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
